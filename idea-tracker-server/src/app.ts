2915fd2bf01a8204c016bb035b313fe89874b3d7
ELFrancisco4 <offiongfrancis14@gmail.com>
2023-05-08 08:32:15 +0100

    Test

--- idea-tracker-server/src/app.ts
+++ idea-tracker-server/src/app.ts
@@ -18,7 +18,7 @@ const allowCrossDomain = function (req, res, next) {
   res.header("Access-Control-Allow-Origin", process.env.ALLOWED_ORIGIN);
   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
   res.header("Access-Control-Allow-Headers", "Content-Type");
-  res.set("Vary", "Origin");
+  res.header("Vary", "Origin");
   next();
 };
 app.use(allowCrossDomain);
